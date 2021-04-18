058f9fc5c0ccaa2f38bc3c09a9d65c0bb6c9bab9
Robin Pillar <rpillar72@gmail.com>
2021-04-18 15:15:52 +0100

    added image to recipe card

--- components/global/RecipeCard.jsx
+++ components/global/RecipeCard.jsx
@@ -9,12 +9,19 @@ import Hard from '../../assets/svg/hard.svg';
 
 import { secondsToTimeString, toNameCase } from '../../utils';
 
-function RecipeCard({ recipe }) {
+function RecipeCard({ recipe: { categories, duration, difficulty, name } }) {
   return (
-    <article className='flex flex-col w-full rounded-10 overflow-hidden shadow-md hover:shadow-2xl transition-shadow duration-500'>
+    <article className='flex flex-col w-full rounded-10 overflow-hidden shadow-md hover:shadow-2xl transition-shadow duration-500 group'>
       <div className='pt-6/12 bg-green-500 relative'>
+        <img
+          src='https://i2.wp.com/completelydelicious.com/wp-content/uploads/2021/02/yeast-raised-doughnuts-6-660x880.jpg'
+          className='object-cover-absolute'
+        />
+
+        <div className='absolute inset-0 bg-gray-900 bg-opacity-20 group-hover:bg-opacity-0 transition-all duration-500'></div>
+
         <div className='flex space-x-10 absolute bottom-0 right-0 mr-10 transform translate-y-6/12'>
-          {recipe.categories.map((category, index) => {
+          {categories.map((category, index) => {
             return <RecipeCategory category={category.slug} key={index} />;
           })}
         </div>
@@ -25,23 +32,25 @@ function RecipeCard({ recipe }) {
             <span className='block w-20 min-w-20 mr-5'>
               <Clock />
             </span>
+
             <span className='text-sm font-light'>
-              {secondsToTimeString(recipe.duration)}
+              {secondsToTimeString(duration)}
             </span>
           </div>
+
           <div className='flex items-center'>
             <span className='block w-20 min-20 mr-5'>
-              {recipe.difficulty === 'easy' && <Easy />}
-              {recipe.difficulty === 'intermediate' && <Intermediate />}
-              {recipe.difficulty === 'hard' && <Hard />}
+              {difficulty === 'easy' && <Easy />}
+              {difficulty === 'intermediate' && <Intermediate />}
+              {difficulty === 'hard' && <Hard />}
             </span>
-            <h6 className='font-light text-sm'>
-              {toNameCase(recipe.difficulty)}
-            </h6>
+
+            <h6 className='font-light text-sm'>{toNameCase(difficulty)}</h6>
           </div>
         </div>
+
         <h3 className='text-3xl font-bold tracking-wider text-gray-800 mt-auto ml-auto'>
-          {recipe.name}
+          {name}
         </h3>
       </div>
     </article>
