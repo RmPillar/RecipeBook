e622acbbc2d47683b166cd268a82f2a7b60231d1
Robin Pillar <rpillar72@gmail.com>
2021-05-15 17:21:54 +0100

    fixed api build issue

--- pages/index.js
+++ pages/index.js
@@ -11,11 +11,16 @@ export default function Home({ data, recipes }) {
   );
 }
 
-export async function getStaticProps(context) {
+export async function getStaticProps({ req }) {
+  let protocol = 'https:';
+  let host = req ? req.headers.host : window.location.hostname;
+  if (host.indexOf('localhost') > -1) {
+    protocol = 'http:';
+  }
   const recipesRes = await fetch(
     `https://recipe-book-be.herokuapp.com/api/recipes`
   );
-  const dataRes = await fetch('/api/home');
+  const dataRes = await fetch(`${protocol}//${host}/api/home`);
 
   const recipes = await recipesRes.json();
   const data = await dataRes.json();
