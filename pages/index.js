a234a597556951e7b33163e41ef32674ffed650f
Robin Pillar <rpillar72@gmail.com>
2021-05-15 21:15:04 +0100

    added console log

--- pages/index.js
+++ pages/index.js
@@ -15,6 +15,7 @@ export async function getStaticProps(context) {
   const recipesRes = await fetch(
     `https://recipe-book-be.herokuapp.com/api/recipes`
   );
+  console.log(process.env.VERCEL_URL);
   const dataRes = await fetch(`${process.env.VERCEL_URL}/api/home`);
 
   const recipes = await recipesRes.json();
